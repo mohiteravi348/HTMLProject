dc6e6bbc6c4ce04f35562a849c38b0cc558b9793
mohiteravi348 <112115134+mohiteravi348@users.noreply.github.com>
2024-10-13 19:02:38 +0530

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,21 @@
+
+var x;
+console.log("before set time out:");
+setTimeout(()=>{
+    console.log("running in set time :");
+},1000);
+
+console.log("After set time out");
+//  x=10;
+console.log(x);
+
+var x;
+
+// var x=10;
+// x=10;
+
+  add();
+
+ function add(){
+    console.log("hello");
+ }
